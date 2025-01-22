a79df17ffab89319a9ee674d356318d952c1e469
Adam Salama <adam.salama@cashflowpositif.fr>
2025-01-22 20:21:39 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Assurez-vous que les chemins correspondent à votre structure de projet
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
